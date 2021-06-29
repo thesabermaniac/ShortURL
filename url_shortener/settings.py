8148b216f053e0bcae2e3fe6b0f86ac88d6f8823
Shalom Vanderhoof <shalomzev@gmail.com>
2021-06-29 23:29:15 +0300

    Updated settings

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2%2f65vu&hjvu)3b1q!d*ve*!t#y-ixyk$=jq=6k5kmq0o^iwm'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
